ac2c6c65567f15fb720202803f701e8f0ffc8320
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2020-07-01 21:53:48 -0300

    validation and error handling

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../tasks.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [
+        TaskStatus.DONE,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.OPEN,
+    ];
+
+    transform(value: any) {
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} is an invalid Status`);
+        }
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const idx = this.allowedStatuses.indexOf(status);
+
+        return idx !== -1;
+    }
+}
