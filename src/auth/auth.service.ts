928f1e3e523c4a7a35fe6dd81d5d78d33c2ae95b
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2020-08-25 12:06:46 -0300

    add signup method on api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './auth.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credntials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
     ) {}
+
+    async signUp(authCrdentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCrdentialsDto);
+    }
 }
